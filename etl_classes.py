24adbbfd53f09aafd0121ff9e8fb62d46528a7af
Sebastiaan Booman <iMeowTwitch@gmail.com>
2023-04-18 12:00:37 +0200

    Added interface paragraph modifier, dynamic etl

--- etl_classes.py
+++ etl_classes.py
@@ -2,6 +2,7 @@ from docx import Document
 import os
 import jsonpickle
 from docxcompose.composer import Composer
+from paragraph_modifier import ParagraphModifier
 import os
 
 #TODO: Abstract class with abc lib? Does not make much sense with static methods
@@ -99,7 +100,88 @@ class DemoETL(AbstractETL):
         json_str = jsonpickle.encode(data)
         with open (dest_str, "w") as f:
             f.write(json_str)
-    
+
+
+
+class DynamicETL(AbstractETL):
+    """ TODO: Docstring """
+    def transform(data, flag_dict={}, ):
+        """ TODO: Docstring """
+
+        for key in flag_dict:
+            if flag_dict[key] is True and key == "stop_words":
+                ParagraphModifier.remove_stop_words()
+            pass
+
+
+        corpora_obj = {}
+        for current_doc, filename in data:
+            print(len(current_doc.paragraphs))
+            corpora : list[list] = []
+            i: int = 0
+            while i < len(current_doc.paragraphs):
+                #Remove Titlepage
+                if current_doc.paragraphs[i].style.name.startswith("Title"):
+                     print("Removing paragraph: Title")
+                     pass
+        
+                #Remove table of contents heading + body
+                elif current_doc.paragraphs[i].text.lower().startswith("inhoudsopgave") or current_doc.paragraphs[i].style.name == "TOCHeading":
+                    print("Removing paragraph: toc")
+                    #TODO: Perhaps table of contents is merged in one paragraph, should check
+                    #Remove next paragraph too
+                    i += 1
+
+                #Remove paragraph headings
+                elif current_doc.paragraphs[i].style.name.startswith("Heading"):
+                     print("Removing paragraph: heading")
+                     #TODO: Perhaps headings can be transformed instead -> may hold value
+                     pass
+                
+                #Remove source list 
+                elif "bibliography" in current_doc.paragraphs[i].style.name.lower() or current_doc.paragraphs[i].text.lower().startswith("literatuurlijst"):
+                     print("Removing paragraph: source list")
+                     pass
+                
+                #Remove table annotations
+                elif False:
+                     print("Removing paragraph: annotation")
+                     #TODO
+                     pass
+                
+                #Remove picture annotations
+                elif False:
+                     print("Removing paragraph: annotation")
+                     #TODO
+                     pass
+                
+                #Remove empty paragraphs
+                elif current_doc.paragraphs[i].text == "":
+                     print("Removing paragraph: empty paragraph") 
+                     #TODO
+                     pass
+                
+                else:
+                    print("Adding text to corpora")
+                    corpora.append([current_doc.paragraphs[i].text, []])
+                i += 1
+            corpora_obj[filename] = corpora
+        return corpora_obj
+
+    @staticmethod
+    def load(dest_str: str, data):
+        """
+        TODO: Docstring
+        """
+        json_str = jsonpickle.encode(data)
+        with open (dest_str, "w") as f:
+            f.write(json_str)
+"""
+
+
+"""
+
+
 class AggregateETL(AbstractETL):
     """ TODO: DOCSTRING """
     @staticmethod
---
+++ paragraph_modifier.py
@@ -0,0 +1,18 @@
+class ParagraphModifier():
+    @staticmethod
+    def lemmatisation(paragraph_text: str) -> str:
+        pass
+
+    @staticmethod
+    def stemming(paragraph_text: str) -> str:
+        pass
+
+    @staticmethod
+    def remove_heading(paragraph_text: dict) -> dict:
+        pass
+
+    @staticmethod
+    def remove_stop_words(stop_words: list[str], paragraph_text: str) -> str:
+        #loop through text, remove words from text that are in stop_words list 
+        #return text
+        pass
