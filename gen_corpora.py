e7b7949f322a42dd688e4786fc65502bb3c0c913
Gaetano Robles <gaetanorobles@gmail.com>
2023-06-17 18:24:23 +0200

    Add google style docstrings

--- gen_corpora.py
+++ gen_corpora.py
@@ -1,10 +1,3 @@
-"""
-Basic ETL proces to create a corpora ready for annotation (NER) step.
-Unoptimized.
-docx data should be stored in `DATA_DIR` destination for file to work
-output directory should be stored in constant `OUTPUT_DIR`
-"""
-
 from datetime import datetime
 from pipeline_config_system import PipelineConfigSystem
 from etl_classes.demo_etl import DemoETL
@@ -14,27 +7,42 @@ from init_source_dest_dir import init_source_dest_dirs
 from data_classes.flag import ParagraphFlags, TextFlags, FlagContainer
 from data_classes.etl_pipelines import PipelineType
 
-#CONSTANTS
+# CONSTANTS
 USE_PIPELINE_CONFIG_SYSTEM = True
 DATA_DIR =  "./data"
 OUTPUT_DIR = "./output"
 
-#FUNCTIONS
 def use_demo_etl():
+    """Executes the DemoETL process to transform data and save the output in JSON format."""
     DemoETL(DATA_DIR, f"{OUTPUT_DIR}/Demo {datetime.now().strftime('%m-%d-%Y,%H-%M-%S')}.json")
 
 def use_dynamic_etl(config: FlagContainer):
+    """
+    Executes the DynamicETL process to dynamically transform data based on the provided configuration
+    and save the output in JSON format.
+
+    Args:
+        config (FlagContainer): Configuration flags for paragraph and text processing.
+    """
     DynamicETL(DATA_DIR, f"{OUTPUT_DIR}/Dyna {datetime.now().strftime('%m-%d-%Y,%H-%M-%S')}.json", config)
 
 def use_aggregate_etl():
+    """Executes the AggregateETL process to aggregate data and save the output in DOCX format."""
     AggregateETL(DATA_DIR, f"{OUTPUT_DIR}/Aggre {datetime.now().strftime('%m-%d-%Y,%H-%M-%S')}.docx")
 
 def execute_pipelines():
+    """
+    Determines and executes the appropriate ETL pipeline based on the configuration.
+
+    The pipeline type is configured using the PipelineConfigSystem. If the pipeline is a default or
+    custom dynamic pipeline, the use_dynamic_etl() function is called with the corresponding flags.
+    If the pipeline is a demo pipeline, the use_demo_etl() function is called.
+    """
     default_paragraph_flags = ParagraphFlags()
     default_text_flags = TextFlags()
     default_flags = FlagContainer(default_paragraph_flags, default_text_flags)
     pipeline, flags = PipelineConfigSystem.configure_pipeline_and_flags(default_flags, USE_PIPELINE_CONFIG_SYSTEM)
-    if pipeline == PipelineType.DEFAULT_DYNAMIC or PipelineType.CUSTOM_DYNAMIC:
+    if pipeline == PipelineType.DEFAULT_DYNAMIC or pipeline == PipelineType.CUSTOM_DYNAMIC:
         use_dynamic_etl(flags)
     elif pipeline == PipelineType.DEMO:
         use_demo_etl()
--- init_source_dest_dir.py
+++ init_source_dest_dir.py
@@ -1,8 +1,17 @@
 import os
 
 def init_source_dest_dirs(src_dir: str, dest_dir: str) -> None:
-    """Checks whether the source and destination folders exist in the project. 
-    If not, then this function creates empty folders"""
+    """
+    Checks whether the source and destination folders exist in the project.
+    If not, creates empty folders.
+
+    Args:
+        src_dir (str): The path to the source directory.
+        dest_dir (str): The path to the destination directory.
+
+    Returns:
+        None
+    """
     source_dir_exists = os.path.isdir(src_dir)
     if not source_dir_exists:
         os.makedirs(src_dir)
